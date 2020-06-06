3ac72afd988a67b62d841456ceff8329439e9679
erangelortega <elianarangelortega@gmail.com>
2020-06-06 16:33:52 -0300

    terminado

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,6 @@
 module Lib where
 import Text.Show.Functions
+import Data.Function
 
 laVerdad = True
 
@@ -23,10 +24,6 @@ todd = UnJugador "Todd" "Ned" (Habilidad 15 80)
 rafa = UnJugador "Rafa" "Gorgory" (Habilidad 10 1)
 
 
--- PUNTO 1 -------------------------
-{-Modelar los palos usados en el juego que a partir de una determinada habilidad 
-generan un tiro que se compone por velocidad, precisión y altura.-}
-
 type Puntos = Int
 
 data Tiro = UnTiro {
@@ -35,10 +32,14 @@ data Tiro = UnTiro {
   altura :: Int
 } deriving (Eq, Show)
 
-type Palo = Habilidad -> Tiro
 
-configurarTiro ::  Int -> Int -> Int -> Tiro 
-configurarTiro velocidad precision altura = UnTiro { velocidad = velocidad, precision = precision, altura = altura }
+-- PUNTO 1 -------------------------
+{-Modelar los palos usados en el juego que a partir de una determinada habilidad 
+generan un tiro que se compone por velocidad, precisión y altura.-}
+
+-- **************************--
+type Palo = Habilidad -> Tiro
+-- **************************--
 
 -- El putter genera un tiro con velocidad igual a 10, el doble de la precisión recibida y altura 0.
 putter :: Palo
@@ -53,107 +54,179 @@ fuerza multiplicada por n, la precisión dividida por n y una altura de n-3 (con
 -}
 
 hierro :: Int -> Palo
-hierro n habilidad = UnTiro { velocidad = (* n).fuerzaJugador $ habilidad, precision = (`div` n).precisionJugador $ habilidad, altura = alturaHierro n }
+hierro n habilidad = UnTiro {
+  velocidad = (* n).fuerzaJugador $ habilidad, 
+  precision = precisionJugador  habilidad `div` n, 
+  altura = max (n - 3) 0 
+  }
 
-alturaHierro :: Int -> Int
-alturaHierro n = max 0  (n-3)
+
+{-Definir una constante palos que sea una lista con todos los palos que se pueden usar en el juego. -}
+palos :: [Palo]
+palos = [putter, madera] ++ map hierro [1 .. 10] 
+
 
 
 -- PUNTO 2
-palos :: [Palo]
-palos = [putter, madera, hierro 1, hierro 2, hierro 3 ,hierro 4, hierro 5, hierro 6, hierro 7, hierro 8, hierro 9, hierro 10]
+{-Definir la función golpe que dados una persona y un palo, obtiene el tiro resultante de usar ese palo con las habilidades de la persona.
+Por ejemplo si Bart usa un putter, se genera un tiro de velocidad = 10, precisión = 120 y altura = 0.-}
 
-golpe :: Palo -> Jugador -> Tiro
-golpe palo = palo.habilidad
+golpe :: Jugador -> Palo -> Tiro
+golpe jugador palo = (palo.habilidad) jugador
 
 
 --PUNTO 3 -----
+
+between :: (Eq a, Ord a) => a -> a -> a -> Bool
+between menor mayor nro = menor <= nro && nro <= mayor
+
+detenerTiro :: Tiro
+detenerTiro = UnTiro 0 0 0
+
+vaAlRasDelSuelo :: Tiro -> Bool
+vaAlRasDelSuelo = (== 0). altura
+
+-- **************************--
+--    VERSION 1 --
+-- **************************--
+
 {-Un túnel con rampita sólo es superado si la precisión es mayor a 90 yendo al ras del suelo, independientemente de la velocidad del tiro. 
 Al salir del túnel la velocidad del tiro se duplica, la precisión pasa a ser 100 y la altura 0. -}
 
-type Obstaculo = Tiro -> Tiro
+data Obstaculo = UnObstaculo {
+  puedeSuperar :: Tiro -> Bool,   {- estoy pasando funciones-}
+  efectoLuegoDeSuperar :: Tiro -> Tiro
+}     {-no pongo deriving porque no voy hacer operaciones con las funciones-}
 
-data TipoObstaculo = TunelConRampita | Laguna {largoLaguna ::Int}| Hoyo deriving (Show, Eq)
+intentarSuperarObstaculo :: Obstaculo -> Tiro -> Tiro
+intentarSuperarObstaculo obstaculo tiroOriginal
+  | (puedeSuperar obstaculo) tiroOriginal = (efectoLuegoDeSuperar obstaculo) tiroOriginal
+  | otherwise = detenerTiro
+
+{-Una laguna es superada si la velocidad del tiro es mayor a 80 y tiene una altura de entre 1 y 5 metros. Luego de superar una laguna 
+el tiro llega con la misma velocidad y precisión, pero una altura equivalente a la altura original dividida por el largo de la laguna.-}
 
 tunelConRampita :: Obstaculo
-tunelConRampita (UnTiro veloc prec alt) 
-  | prec > 90 && alt == 0 = UnTiro {velocidad = veloc * 2, precision = 100, altura = 0 }
-  | otherwise = detener
+tunelConRampita = UnObstaculo superaTunelConRampita efectoTunelConRampita
+
+superaTunelConRampita :: Tiro -> Bool
+superaTunelConRampita tiro = precision tiro > 90 && vaAlRasDelSuelo tiro
+
+efectoTunelConRampita :: Tiro -> Tiro
+efectoTunelConRampita tiroOriginal = tiroOriginal {
+  velocidad = velocidad tiroOriginal * 2,
+  precision = 100,
+  altura = 0
+  }
 
 {-Una laguna es superada si la velocidad del tiro es mayor a 80 y tiene una altura de entre 1 y 5 metros. Luego de superar una laguna 
 el tiro llega con la misma velocidad y precisión, pero una altura equivalente a la altura original dividida por el largo de la laguna.-}
 
 laguna :: Int -> Obstaculo
-laguna largoLaguna (UnTiro veloc prec alt)
-  | veloc > 80 && (alt >= 1 && alt <= 5) = UnTiro {velocidad = veloc, precision = prec, altura = div alt largoLaguna }
-  | otherwise = detener
+laguna largoLaguna = UnObstaculo superaLaguna (efectoLaguna largoLaguna)
+
+superaLaguna :: Tiro -> Bool
+superaLaguna tiro = velocidad tiro > 80 && (between 1 5 . altura ) tiro
+
+efectoLaguna :: Int -> Tiro -> Tiro
+efectoLaguna largoLaguna tiroOriginal = tiroOriginal { altura = altura tiroOriginal `div` largoLaguna }
 
 {-Un hoyo se supera si la velocidad del tiro está entre 5 y 20 m/s yendo al ras del suelo con una precisión mayor a 95.
  Al superar el hoyo, el tiro se detiene, quedando con todos sus componentes en 0.-}
 
 hoyo :: Obstaculo
-hoyo (UnTiro veloc prec alt)
-  | (veloc >= 5 && veloc <= 20) && prec > 95 && alt == 0 = detener 
-  | otherwise = detener
+hoyo = UnObstaculo superaHoyo efectoHoyo
 
-detener :: Tiro
-detener = UnTiro 0 0 0
+superaHoyo :: Tiro -> Bool
+superaHoyo tiro = between 5 20 (velocidad tiro) && vaAlRasDelSuelo tiro && precision tiro > 95
 
-obstaculoSuperado :: TipoObstaculo -> Tiro -> Tiro
-obstaculoSuperado TunelConRampita tiro = tunelConRampita tiro
-obstaculoSuperado (Laguna largoLaguna) tiro = laguna largoLaguna tiro
-obstaculoSuperado Hoyo tiro = hoyo tiro
+efectoHoyo :: Tiro -> Tiro
+efectoHoyo _ = detenerTiro
+
+-- **************************--
+--    VERSION 2 --
+-- **************************--
+type ObstaculoV2 = Tiro -> Tiro
+
+obstaculoSuperableSi :: (Tiro -> Bool) -> (Tiro -> Tiro) -> ObstaculoV2
+obstaculoSuperableSi condicion efecto tiroOriginal 
+  | condicion tiroOriginal = efecto tiroOriginal
+  | otherwise = detenerTiro
+
+
+tunelConRampita2 :: ObstaculoV2 
+tunelConRampita2 = obstaculoSuperableSi superaTunelConRampita efectoTunelConRampita 
+
+laguna2 :: Int -> ObstaculoV2 
+laguna2 largoLaguna = obstaculoSuperableSi superaLaguna (efectoLaguna largoLaguna)
+
+hoyo2 :: ObstaculoV2 
+hoyo2 = obstaculoSuperableSi superaHoyo efectoHoyo
+
+
 
 -- PUNTO 4 ----------
-{-Definir palosUtiles que dada una persona y un obstáculo, permita determinar qué palos le sirven para superarlo-}
 
-palosUtiles :: Persona -> 
+-- **************************--
+--    VERSION 1 --
+-- **************************--
+{-a) Definir palosUtiles que dada una persona y un obstáculo, permita determinar qué palos le sirven para superarlo-}
 
+palosUtiles :: Jugador -> Obstaculo -> [Palo]
+palosUtiles jugador obstaculo = filter (paloSirve obstaculo jugador) palos
 
-
-{-type Obstaculo = Type -> Bool
-efectoObstaculo :: Obstaculo -> Tiro -> Tiro
-efectoObstaculo obstaculoTunel tiro  = cnjwcenwkec
-efectoObstaculo (obstaculoLaguna long) = 
-
-alRazDelSuelo :: Tiro -> Bool
-alRazDelSuelo = (==0).altura 
-
-gra
-
-tunel :: 
-tunel =
-
-laguna :: Tiro -> Obstaculo
-laguna =
-
-hoyo :: Obstaculo
-hoyo =
-
-comoQueda laguna tiro | f tiro =
-                      |
-comoQueda tunel
+paloSirve :: Obstaculo -> Jugador -> Palo -> Bool
+paloSirve obstaculo jugador = puedeSuperar obstaculo . golpe jugador
 
 
-type Obstaculo = Tiro -> Bool
+-- **************************--
+--    VERSION 2 -- crearon el DATA como en la version 1 y terminaron igual
+-- **************************--
 
--- Un túnel con rampita sólo es superado si la precisión es mayor a 90 yendo al ras del suelo, 
--- independientemente de la velocidad del tiro. Al salir del túnel la velocidad del tiro se duplica, 
--- la precisión pasa a ser 100 y la altura 0.
+{-b) Saber, a partir de un conjunto de obstáculos y un tiro, cuántos obstáculos consecutivos se pueden superar.-}
 
-rampita :: Obstaculo
-rampita tiro = (>90)(precision tiro) && (==0) (altura tiro)
+cantObstaculosConsecutivosSuperados :: Tiro -> [Obstaculo] -> Int {-con recursividad-}
+cantObstaculosConsecutivosSuperados tiro [] = 0
+cantObstaculosConsecutivosSuperados tiro (obstaculo : obstaculos)
+  | puedeSuperar obstaculo tiro = 1 + cantObstaculosConsecutivosSuperados (efectoLuegoDeSuperar obstaculo tiro) obstaculos
+  | otherwise = 0
 
--- Una laguna es superada si la velocidad del tiro es mayor a 80 y tiene una altura de entre 1 y 5 metros. 
--- Luego de superar una laguna el tiro llega con la misma velocidad y precisión, 
--- pero una altura equivalente a la altura original dividida por el largo de la laguna.
+-- **************************--
+--    Bonus
+-- **************************--
+cantObstaculosConsecutivosSuperados' :: Tiro -> [Obstaculo] -> Int {-sin recursividad-}
+cantObstaculosConsecutivosSuperados' tiro obstaculos 
+  = (length . takeWhile (\(obstaculo , tiroQueLlega) -> puedeSuperar obstaculo tiroQueLlega) . zip obstaculos . tirosSucesivos tiro) obstaculos 
 
-laguna :: Int -> Obstaculo 
-laguna altLaguna tiro= (>80)(velocidad tiro) && (>1)(altura tiro) && (<5)(altura tiro)
+tirosSucesivos :: Tiro -> [Obstaculo] -> [Tiro]
+tirosSucesivos tiroOriginal obstaculos 
+  = foldl (\tirosGenerados obstaculo -> tirosGenerados ++ [efectoLuegoDeSuperar obstaculo (last tirosGenerados)]) [tiroOriginal] obstaculos
+  
+{-Definir paloMasUtil que recibe una persona y una lista de obstáculos y determina cuál es el palo que le permite superar más obstáculos con un solo tiro.-}
+maximoSegun :: Ord b => (a -> b) -> [a] -> a
+maximoSegun f = foldl1 (mayorSegun f)
 
--- Un hoyo se supera si la velocidad del tiro está entre 5 y 20 m/s yendo al ras del suelo con una precisión mayor a 95. 
--- Al superar el hoyo, el tiro se detiene, quedando con todos sus componentes en 0.
+mayorSegun :: Ord x => (t -> x) -> t -> t -> t
+mayorSegun f a b
+  | f a > f b = a
+  | otherwise = b
 
-hoyo :: Obstaculo
-hoyo tiro = between 5 20 (velocidad tiro) && (==0)(altura tiro) && (>95)(precision tiro)-}
+paloMasUtil :: Jugador -> [Obstaculo] -> Palo
+paloMasUtil jugador obstaculos
+  = maximoSegun (flip cantObstaculosConsecutivosSuperados obstaculos . golpe jugador) palos
 
+-- PUNTO --
+{-Dada una lista de tipo [(Jugador, Puntos)] que tiene la información de cuántos puntos ganó cada niño al finalizar el torneo, 
+se pide retornar la lista de padres que pierden la apuesta por ser el “padre del niño que no ganó”. Se dice que un niño ganó el torneo 
+si tiene más puntos que los otros niños.-}
+
+jugadorDeTorneo = fst
+puntosGanados = snd
+
+pierdenLaApuesta :: [(Jugador, Puntos)] -> [String]
+pierdenLaApuesta puntosDelTorneo 
+  = ( map (padre.jugadorDeTorneo) . filter ( not.gano puntosDelTorneo)) puntosDelTorneo
+
+gano :: [(Jugador, Puntos)] -> (Jugador, Puntos) -> Bool
+gano puntosDelTorneo puntosDelJugador 
+  = (all ((< puntosGanados puntosDelJugador) .puntosGanados) . filter (/= puntosDelJugador)) puntosDelTorneo
